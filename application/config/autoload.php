f0323fef2c951535e55ca96255e22abea74423f8
Katiuska Ramirez <infor.soport@gmail.com>
2013-11-18 20:18:13 -0430

    configuracion autoload/database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array();
+$autoload['model'] = array('dispositivos', 'localidad', 'interfaz', 'radio');
 
 
 /* End of file autoload.php */
