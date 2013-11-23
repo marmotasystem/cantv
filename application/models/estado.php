3ff52d26ebbddd34db428bda15aafc9961f423ba
daniel <xzdasx@gmail.com>
2013-11-23 15:15:17 -0430

    Consulta de estados

--- application/models/estado.php
+++ application/models/estado.php
@@ -7,13 +7,33 @@ class Estado extends CI_Model
     
   public function __construct()
   {
-   parent::__construct();
-   self::$db = &get_instance()->db;
+    parent::__construct();
+    self::$db = &get_instance()->db;
   }
 
   public static function all()
   {
-  
+    $sql = self::$querys['all']; 
+    $query = self::$db->query($sql);
+    $results = array();
+    $results_objects = array();
+    foreach ($query->result_array() as $row)
+    {
+      $results[] = $row;
+    }
+    foreach ($results as $estado_array)
+    {
+      $object = new Estados();
+      $object->set_nombre($estado_array['nombre']);
+      $object->set_id($estado_array['id']);
+      $results_objects[] = $object;
+    }
+    return $results_objects;
   }
+
+
+  private static $querys = array(
+    'all' => 'SELECT id,nombre FROM Estados'
+  );
 
 }  
