c644aec1f162e4fab03972188fc9f1e3e2accf63
daniel <xzdasx@gmail.com>
2013-11-23 19:49:33 -0430

    formulario

--- application/views/localidad/formulario.php
+++ application/views/localidad/formulario.php
@@ -1,15 +1,16 @@
-<?= form_open('localidades/create');?>
+<?php $accion = ( $localidad->id != null )? 'actualizar' : 'create' ?>
+<?= form_open('localidades/'.$localidad,'',array('id'=>$localidad->id));?>    
   <h2>Formulario de Registro Localidad</h2>
   <fieldset width="80%">
     <legend>Introduzca los Datos</legend>
     <div>      
       <label>Estado: </br>
-        <?= form_dropdown('localidad[estado_id]', $estados);?>
+        <?= form_dropdown('localidad[estado_id]', $estados,$localidad->estado_id);?>
       </label>
     </div>      
     <div>    
       <label>Localidad
-        <input id="nom_localidad" type="text" name="localidad[nombre]" placeholder="Ingrese Nombre..." required>
+        <input id="nom_localidad" type="text" name="localidad[nombre]" placeholder="Ingrese Nombre..." required value="<?= $localidad->nombre?>">
       </label>
     </div>
     <div>
