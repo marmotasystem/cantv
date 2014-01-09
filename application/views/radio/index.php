9933efda8f6c69ab52e06565f9bdde519d2c1d2b
Katiuska Ramirez <infor.soport@gmail.com>
2014-01-08 21:08:31 -0430

    se agrego link para enlaces

--- application/views/radio/index.php
+++ application/views/radio/index.php
@@ -15,6 +15,7 @@
         <td> <?= $radio->localidad() ?></td>
         <td> <?= $radio->nombre ?></td>
         <td> <?= anchor('radios/editar/'.$radio->id, "Editar"); ?> </td>
+        <td> <?= anchor('radios/enlaces/'.$radio->id, "Enlaces"); ?> </td>
       </tr>
     <? endforeach ?>
   </tbody>
