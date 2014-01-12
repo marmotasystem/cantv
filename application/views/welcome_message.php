7dac31db8493858c0b74000c406fc59b43a28643
Katiuska Ramirez <infor.soport@gmail.com>
2014-01-11 21:14:08 -0430

    eliminado los stilos

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,24 +1,12 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="<?=base_url()?>js/bootstrap.min.js"></script>
-    <title>Bootstrap 101 Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="<?=base_url()?>css/bootstrap.min.css">
-
-  </head>
-<h3>
-<p align="center"><font color="Blue">Gerencia Gestion de Trafico</font></br>
-<p align="center"><font color="Blue">Coordinacion Gestion Productos Red de Datos</font></br></h3>
+<h3>Gerencia Gestion de Trafico </h3>
+<h3>Coordinacion Gestion Productos Red de Datos </h3>
 </br>
-<h4><center><font color="Blue">Interfaces - Dispositivos NGN/MetroEthernet/ABA</font></center></h4>
+<h4>Interfaces - Dispositivos NGN/MetroEthernet/ABA</h4>
 </br>
-<center><TABLE BORDER=1 WIDTH=500>
+<TABLE BORDER=1 WIDTH=500>
 	<td WIDTH=100><?= img('img/logo.gif')?></td>
 	<td WIDTH=100><?= img('img/logo_aba.gif')?></td>
 	<td WIDTH=100><?= img('img/logo_metro.gif')?></td>
-	<td WIDTH=100><?= img('img/logo_cgprd.gif')?></td>
-</table></center></br>
+	﻿<td WIDTH=100><?= img('img/logo_cgprd.gif')?></td>
+</table>
 
