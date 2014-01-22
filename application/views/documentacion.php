f106799297c2aa7f46454df5e78a6a41415fff08
Katiuska Ramirez <infor.soport@gmail.com>
2014-01-21 23:22:57 -0430

    documentacion

---
+++ application/views/documentacion.php
@@ -0,0 +1,8 @@
+<h2>
+ 
+ Listado de Documentos
+
+</h2>
+
+ <a href= "C:\wamp\www\proyecto_cantv\documentos\Descripcion de NGN.pdf"> 1.- Descripcion de NGN</a></br>
+ <a href= "C:\wamp\www\proyecto_cantv\documentos\IntroduccionNGN.pdf"> 2.- Introduccion NGN</a></br>
---
+++ application/views/radio/editar.php
@@ -0,0 +1,5 @@
+<h2>
+  Editar Radio
+</h2>
+
+<? include('formulario.php') ?>
